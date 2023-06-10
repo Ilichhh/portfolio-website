2954b57cfebb13cf656652301e58beb02652c878
ilya <shakurov.ia@gmail.com>
2023-06-10 14:21:56 +0300

    feat: add styles and buttons to hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,46 @@
+import { Link } from 'react-router-dom';
 import { Container } from './common';
 
 import styled from 'styled-components';
 import theme from '../theme';
 
 const Section = styled.section`
-  height: 100vh;
+  height: calc(100vh - 70px);
+  background-color: ${theme.colors.bgDark};
+`;
+
+const Header = styled.h1`
+  margin: 80px 0 20px;
+  font-size: 48px;
+  color: ${theme.colors.green};
+`;
+
+const Subheader = styled.h2`
+  width: 50%;
+  font-size: 24px;
+  margin-bottom: 60px;
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 40px;
+`;
+
+const Button = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  width: 100px;
+  border: 1px solid ${theme.colors.important};
+  border-radius: 4px;
+  color: ${theme.colors.bgDark};
+  background-color: ${theme.colors.important};
+  cursor: pointer;
+`;
+
+const ButtonOutlined = styled(Button)`
+  color: ${theme.colors.important};
   background-color: ${theme.colors.bgDark};
 `;
 
@@ -12,15 +48,20 @@ export const HeroSection = () => {
   return (
     <Section>
       <Container>
-        <h1>
+        <Header>
           Hi,
           <br />
           I&apos;m Ilya,
           <br />
           Front End Developer
-        </h1>
-        <h2>I’m a design oriented software engineer. Experienced in blockchain.</h2>
-        <button>HIRE ME!</button>
+        </Header>
+        <Subheader>I’m a design oriented software engineer. Experienced in blockchain.</Subheader>
+        <ButtonsWrapper>
+          <Button to="/cv.pdf" target="_blank">
+            CV
+          </Button>
+          <ButtonOutlined to="mailto:shakurov.ia@gmail.com">Contact</ButtonOutlined>
+        </ButtonsWrapper>
       </Container>
     </Section>
   );
