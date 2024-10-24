fbbdb35629b821e4d29c33af1cb2ce31fb1cd588
Ilya <shakurov.77@mail.ru>
2024-10-24 15:48:42 +0300

    feat: update buttons styles

--- src/components/common/Buttons.tsx
+++ src/components/common/Buttons.tsx
@@ -1,36 +1,20 @@
 import { Link } from 'react-router-dom';
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import theme from '../../theme';
 
-export const LinkButton = styled(Link)<{ color?: string; width?: number }>`
+interface ButtonProps {
+  color?: string;
+  width?: number;
+  centered?: boolean;
+}
+
+export const BasicButtonStyles = css<ButtonProps>`
   display: flex;
   justify-content: center;
   align-items: center;
   align-self: flex-start;
   gap: 8px;
-  height: 50px;
-  width: ${({ width }) => width}px;
-  padding: 20px;
-  text-decoration: none;
-  font-weight: 600;
-  border: 4px solid ${theme.colors.textDark};
-  color: ${theme.colors.textDark};
-  background-color: ${({ color }) => color};
-  box-shadow: 6px 6px ${theme.colors.textDark};
-  cursor: pointer;
-  transition: 0.2s all;
-  &:hover {
-    transform: translate(-3px, -3px);
-    box-shadow: 9px 9px;
-  }
-`;
-
-export const Button = styled.button<{ color?: string; width?: number; centered?: boolean }>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-self: flex-start;
   height: 50px;
   width: ${({ width }) => width}px;
   padding: 20px;
@@ -46,4 +30,17 @@ export const Button = styled.button<{ color?: string; width?: number; centered?:
     transform: translate(-3px, -3px);
     box-shadow: 9px 9px;
   }
+  &: active {
+    transform: translate(6px, 6px);
+    box-shadow: 0px 0px;
+  }
+`;
+
+export const Button = styled.button<ButtonProps>`
+  ${BasicButtonStyles};
+`;
+
+export const LinkButton = styled(Link)<ButtonProps>`
+  ${BasicButtonStyles};
+  text-decoration: none;
 `;
